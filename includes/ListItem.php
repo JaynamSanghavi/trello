8c7e357ef7e14db3a78fbb063e8c46f8054aadee
JaynamSanghavi <jaynam.s@somaiya.edu>
2019-10-09 20:17:11 +0530

    session updated

--- includes/ListItem.php
+++ includes/ListItem.php
@@ -3,9 +3,10 @@
     include_once("../classes/Functions.class.php");
     
     if(isset($_POST['submit'])){
+        echo "hello";
         extract($_POST);
-        $newListItem = new ListItem();
-        $newListItem->insertListItem($list_item_title,$list_item_content,$list_index,$author_id);
-        Functions::redirect("../index.php");
+        // $newListItem = new ListItem();
+        // $newListItem->insertListItem($list_item_title,$list_item_content,$list_index,$author_id);
+        // Functions::redirect("../index.php");
     }
 ?>
