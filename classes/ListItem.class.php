8b48dbdf543a7fb0e59e406ec3877ea21f9f30c6
JaynamSanghavi <jaynam.s@somaiya.edu>
2019-10-09 20:23:16 +0530

    session updated

--- classes/ListItem.class.php
+++ classes/ListItem.class.php
@@ -9,13 +9,14 @@
 
         public function insertListItem($list_item_title,$list_item_content,$list_index,$author_id){
             $query = "INSERT INTO list_item (list_id, list_item_title, list_item_content, author_id) VALUES (?,?,?,?)";
-            $preparedStatement = $this->connection->prepare($query);
-            $preparedStatement->bind_param("issi",$list_index,$list_item_title,$list_item_content,$author_id);
-            if($preparedStatement->execute()){
-                return $this->connection->insert_id;
-            } else{
-                die("ERROR WHILE INSERTING STUDENT");
-            }
+            echo $query;
+            // $preparedStatement = $this->connection->prepare($query);
+            // $preparedStatement->bind_param("issi",$list_index,$list_item_title,$list_item_content,$author_id);
+            // if($preparedStatement->execute()){
+            //     return $this->connection->insert_id;
+            // } else{
+            //     die("ERROR WHILE INSERTING STUDENT");
+            // }
         }
 
         public function readListItem($list_id){
