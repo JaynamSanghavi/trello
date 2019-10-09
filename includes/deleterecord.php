4e88dfcb87ab9c7a6fd992c9fee498a9ba37d218
JaynamSanghavi <jaynam.s@somaiya.edu>
2019-10-09 23:38:27 +0530

    mod edit

--- includes/deleterecord.php
+++ includes/deleterecord.php
@@ -2,12 +2,10 @@
     include_once("../classes/ListItem.class.php");
     include_once("../classes/Functions.class.php");
     if(isset($_GET['id'])){
-        extract($_GET);
-        // $newListItem = new ListItem();
-        // $newListItem->deleteItem($id);
+         $newListItem = new ListItem();
+         $newListItem->deleteItem($_GET['id']);
         //Functions::redirect("../index.php");
 
     }else{
         echo "die";
-    }
-?>
+  
